5d38ac5e00612168a45bef9fb3a3a6ea14e4e778
Eliorika <arina.pavlovna.20@gmail.com>
2024-01-02 20:00:19 +0300

    init

---
+++ src/main/java/ru/rsreu/ChineseCourse/ChineseCourseApplication.java
@@ -0,0 +1,13 @@
+package ru.rsreu.ChineseCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChineseCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChineseCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rsreu/ChineseCourse/ChineseCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rsreu.ChineseCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChineseCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
